c697c00cf158b83329db41d5664d2bd328ed0122
rizkyrajitha <rajithagunathilake@gmail.com>
2020-08-23 20:42:02 +0530

    added few more feilds

--- index.js
+++ index.js
@@ -9,10 +9,60 @@ async function run() {
     const CUSTOMMESSAGE =
       core.getInput("CUSTOMMESSAGE") || "Successfully Completed Github Action";
     const STATUS = core.getInput("STATUS") || true;
+    //${{ job.status }}
+    const JOBSTATUS = core.getInput("JOBSTATUS") || false;
 
-    let msg = STATUS==='true'
-      ? CUSTOMMESSAGE + " \xE2\x9C\x85"
-      : CUSTOMMESSAGE + " \xE2\x9D\x8C";
+    // : "${env.GITHUB_SERVER_URL}/${env.GITHUB_REPOSITORY}/actions/runs/${env.GITHUB_RUN_ID}"}]
+    const GITHUB_RUN_NUMBER = core.getInput("GITHUB_RUN_NUMBER") || 0;
+    const GITHUB_REPOSITORY = core.getInput("GITHUB_REPOSITORY") || "";
+    const GITHUB_RUN_ID = core.getInput("GITHUB_RUN_ID") || 0;
+    const GITHUB_ACTOR = core.getInput("GITHUB_ACTOR") || "";
+    const GITHUB_EVENT_NAME = core.getInput("GITHUB_EVENT_NAME") || "";
+    const GITHUB_SHA = core.getInput("GITHUB_SHA") || 0;
+
+    let msg =
+      STATUS === "true"
+        ? `${CUSTOMMESSAGE} \xE2\x9C\x85 ${
+            JOBSTATUS ? "Job status : " + JOBSTATUS + "\n" : ""
+          } ${
+            GITHUB_RUN_NUMBER
+              ? "GitHub Run Number : " + GITHUB_RUN_NUMBER + "\n"
+              : ""
+          } ${
+            GITHUB_REPOSITORY
+              ? "GitHub Repository : " + GITHUB_REPOSITORY + "\n"
+              : ""
+          }  
+          ${GITHUB_RUN_ID ? "GitHub Run ID : " + GITHUB_RUN_ID + "\n" : ""}  
+          ${GITHUB_ACTOR ? "Triggered user : " + GITHUB_ACTOR + "\n" : ""} 
+          ${
+            GITHUB_EVENT_NAME
+              ? "Triggered event : " + GITHUB_EVENT_NAME + "\n"
+              : ""
+          } 
+          ${GITHUB_SHA ? "GitHub commit SHA : " + GITHUB_SHA + "\n" : ""} 
+
+          `
+        : ` ${CUSTOMMESSAGE}  " \xE2\x9D\x8C  ${
+            JOBSTATUS ? "Job status : " + JOBSTATUS + "\n" : ""
+          } ${
+            GITHUB_RUN_NUMBER
+              ? "GitHub Run Number : " + GITHUB_RUN_NUMBER + "\n"
+              : ""
+          } ${
+            GITHUB_REPOSITORY
+              ? "GitHub Repository : " + GITHUB_REPOSITORY + "\n"
+              : ""
+          } 
+        ${GITHUB_RUN_ID ? "GitHub Run ID : " + GITHUB_RUN_ID + "\n" : ""}  
+        ${GITHUB_ACTOR ? "Triggered user : " + GITHUB_ACTOR + "\n" : ""} 
+        ${
+          GITHUB_EVENT_NAME
+            ? "Triggered event : " + GITHUB_EVENT_NAME + "\n"
+            : ""
+        } 
+        ${GITHUB_SHA ? "GitHub commit SHA : " + GITHUB_SHA + "\n" : ""} 
+`;
 
     let data = await fetch(
       `https://api.telegram.org/bot${TELEGRAM_BOT_ID}/sendMessage?chat_id=${TELEGRAM_CHAT_ID}&text=${msg}`,
